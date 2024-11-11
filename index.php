b15af7d7ee5ba0ecfc6c70f072b523e9a25ae4f4
Nathan <133197161+SirFaNaTiC@users.noreply.github.com>
2024-11-11 14:52:49 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include 'includes/head.php'?>
+</head>
+
+<body>
+    <!-- En Tête -->
+    <header class="header">
+        <?php include "includes/header.php"?>
+    </header>
+
+    <!-- Partie Centrale -->
+    <main>
+        <div class="liste_produit">
+            <h2> Produits : </h2>
+        </div>
+    </main>
+
+    <!-- Footer -->
+    <?php include 'includes/footer.php'?>
+    
+</body>
+</html>
